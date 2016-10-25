a66ad59cab56de1aa2caa7424c539c68092613ca
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-10-25 19:39:41 +0700

    Embedded http://tryroslyn.azurewebsites.net in vscode (￣･_______･￣)

--- previewhtml-sample/src/extension.ts
+++ previewhtml-sample/src/extension.ts
@@ -21,50 +21,29 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         public update(uri: vscode.Uri) {
-            this._onDidChange.fire(uri);
+            //this._onDidChange.fire(uri);
         }
 
         private createCssSnippet() {
-            let editor = vscode.window.activeTextEditor;
-            if (!(editor.document.languageId === 'css')) {
-                return this.errorSnippet("Active editor doesn't show a CSS document - no properties to preview.")
-            }
-            return this.extractSnippet();
-        }
-
-        private extractSnippet(): string {
-            let editor = vscode.window.activeTextEditor;
-            let text = editor.document.getText();
-            let selStart = editor.document.offsetAt(editor.selection.anchor);
-            let propStart = text.lastIndexOf('{', selStart);
-            let propEnd = text.indexOf('}', selStart);
-
-            if (propStart === -1 || propEnd === -1) {
-                return this.errorSnippet("Cannot determine the rule's properties.");
-            } else {
-                return this.snippet(editor.document, propStart, propEnd);
-            }
-        }
-
-        private errorSnippet(error: string): string {
-            return `
-                <body>
-                    ${error}
-                </body>`;
-        }
-
-        private snippet(document: vscode.TextDocument, propStart: number, propEnd: number): string {
-            const properties = document.getText().slice(propStart + 1, propEnd);
-            return `<style>
-                    #el {
-                        ${properties}
-                    }
-                </style>
-                <body>
-                    <div>Preview of the <a href="${encodeURI('command:extension.revealCssRule?' + JSON.stringify([document.uri, propStart, propEnd]))}">CSS properties</a></dev>
-                    <hr>
-                    <div id="el">Lorem ipsum dolor sit amet, mi et mauris nec ac luctus lorem, proin leo nulla integer metus vestibulum lobortis, eget</div>
-                </body>`;
+          return `
+                <html>
+                    <head>
+                        <style>
+                            body {
+                            }
+                            iframe {
+                                position: absolute; 
+                                width: 100%;
+                                height: 100%;
+                                border: none;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <iframe src="http://tryroslyn.azurewebsites.net"/>
+                    </body>
+                </html>
+            `;
         }
     }
 
@@ -84,7 +63,7 @@ export function activate(context: vscode.ExtensionContext) {
     })
 
     let disposable = vscode.commands.registerCommand('extension.showCssPropertyPreview', () => {
-        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'CSS Property Preview').then((success) => {
+        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'CSS Property Preview').then((success) => {
         }, (reason) => {
             vscode.window.showErrorMessage(reason);
         });
