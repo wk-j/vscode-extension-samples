850ec83278c8408a1de16eab4c3066470ff2ce7b
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-10-25 20:21:46 +0700

    Embedded https://learnxinyminutes.com ◕‿◕

--- previewhtml-sample/src/extension.ts
+++ previewhtml-sample/src/extension.ts
@@ -30,17 +30,23 @@ export function activate(context: vscode.ExtensionContext) {
                     <head>
                         <style>
                             body {
+                                padding: 0;
+                                background: white;
                             }
                             iframe {
                                 position: absolute; 
                                 width: 100%;
                                 height: 100%;
                                 border: none;
+                                margin: 0 auto;
+
+                                -webkit-transform:scale(0.85);
+                                -moz-transform-scale(0.85);
                             }
                         </style>
                     </head>
                     <body>
-                        <iframe src="http://tryroslyn.azurewebsites.net"/>
+                        <iframe src="https://learnxinyminutes.com"/>
                     </body>
                 </html>
             `;
@@ -50,38 +56,11 @@ export function activate(context: vscode.ExtensionContext) {
     let provider = new TextDocumentContentProvider();
     let registration = vscode.workspace.registerTextDocumentContentProvider('css-preview', provider);
 
-    vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
-        if (e.document === vscode.window.activeTextEditor.document) {
-            provider.update(previewUri);
-        }
-    });
-
-    vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
-        if (e.textEditor === vscode.window.activeTextEditor) {
-            provider.update(previewUri);
-        }
-    })
-
     let disposable = vscode.commands.registerCommand('extension.showCssPropertyPreview', () => {
-        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'CSS Property Preview').then((success) => {
+        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'CSS Property Preview').then((success) => {
         }, (reason) => {
             vscode.window.showErrorMessage(reason);
         });
-    });
-
-    let highlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(200,200,200,.35)' });
-
-    vscode.commands.registerCommand('extension.revealCssRule', (uri: vscode.Uri, propStart: number, propEnd: number) => {
-
-        for (let editor of vscode.window.visibleTextEditors) {
-            if (editor.document.uri.toString() === uri.toString()) {
-                let start = editor.document.positionAt(propStart);
-                let end = editor.document.positionAt(propEnd + 1);
-
-                editor.setDecorations(highlight, [new vscode.Range(start, end)]);
-                setTimeout(() => editor.setDecorations(highlight, []), 1500);
-            }
-        }
     });
 
     context.subscriptions.push(disposable, registration);
